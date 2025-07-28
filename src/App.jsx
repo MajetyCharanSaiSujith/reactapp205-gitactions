a79b088e13b0019500d9fc3eb2a3304b1702f939
CharanSai <majetycharansai2006@gmail.com>
2025-07-28 10:25:39 +0530

    updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,10 @@ import './style.css';
 
 function App() {
   return (
+    
     <div className="container">
-      <h1>Welcome to My React Project - KIRAN</h1>
+      <h1>Welcome to My React Project - Charan</h1>
+      <p>CI/CD Cloud DevOps</p>
       <p>This is a simple homepage deployed on GitHub Pages using Vite.</p>
     </div>
   );
